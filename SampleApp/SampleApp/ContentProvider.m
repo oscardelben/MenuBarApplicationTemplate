cbf2aaf262017246be580d18e46b5bb2aef20f2d
Peter Jenkins <peter@funroll.co>
2014-05-29 13:30:15 -0400

    Add ContentProvider class (unmodified.)

---
+++ SampleApp/SampleApp/ContentProvider.m
@@ -0,0 +1,13 @@
+//
+//  ContentProvider.m
+//  SampleApp
+//
+//  Created by Jenkins, Peter on 5/29/14.
+//  Copyright (c) 2014 DibiStore. All rights reserved.
+//
+
+#import "ContentProvider.h"
+
+@implementation ContentProvider
+
+@end
